84dc3ddbc3b9c9d17c0403525d246cf5c99383aa
Chris Hackbardt <christopherhackbardt@yahoo.com>
2022-09-16 16:48:08 -0400

    added functions

---
+++ matlabCode/earthOrbitAltitudes.m
@@ -0,0 +1,7 @@
+function [a e p h va vp] = earthOrbitAltitudes(ra,rp)
+    %This function calculates properties of a earth orbit with inputs of altitude at apoapsis and periapsis
+    %Function call: [a e p h va vp] = earthOrbitAltitudes(ra,rp)
+    %Input: ra, radius at apoapsis
+    %Input: rp, radius at periapsis
+    %
+end
---
+++ matlabCode/testFun.m
@@ -0,0 +1,11 @@
+function y=testFun(x)
+
+
+%This funtion computes the square of the input
+%Function call: y = testFun(X)
+%Input: x (real number)
+%Output: y (real number)
+
+
+y=x^2;
+end
